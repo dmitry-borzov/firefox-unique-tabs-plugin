112efef3a6afd0d1f85d5a8d129b8b1115eced84
dmitry-borzov <d.borzov@belitsoft.com>
2017-11-29 11:46:23 +0300

    Fix typo

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 'use strict';
 
 browser.browserAction.onClicked.addListener(function(){
-    var gettingActiveTab = browser.tabs.query({currentWindow: true});
-    gettingActiveTab.then(function (tabs) {
+    browser.tabs.query({currentWindow: true}).then(function (tabs) {
         if(!tabs.length) return;
         let tabUrls = tabs.map(function(_){ return _.url;});
         let uniqueUrls = tabUrls.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
